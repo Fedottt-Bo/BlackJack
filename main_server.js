3bd46e145f31bd011e22e10394ae0f4162d81729
Fedottt-Bo <77093554+Fedottt-Bo@users.noreply.github.com>
2021-06-18 11:15:28 +0300

    Server file created

---
+++ main_server.js
@@ -0,0 +1,14 @@
+const express = require('express');
+const favicon = require('serve-favicon')
+const app = express();
+const http = require('http');
+const fs = require('fs');
+var cookieParser = require('cookie-parser');
+
+const server = http.createServer(app);
+const { Server } = require("socket.io");
+const io = new Server(server);
+
+var debug = fs.createWriteStream('./!logs/errors.log');
+process.stdout.write = debug.write.bind(debug);
+
